1e7bb46d13f93eae65be29d553903fd931258357
yurigligoric <yurigligoric@gmail.com>
2023-01-08 23:57:29 +0800

    first commit setting up

---
+++ Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReviewApp.Models;
+
+namespace ReviewApp.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Pokemon> Pokemons { get; set; }
+        public DbSet<PokemonCategory> PokemonCategories { get; set; }
+        public DbSet<PokemonOwner> PokemonOwners { get; set; }  
+
+        public DbSet<Review> Reviews { get; set; }  
+        public DbSet<Reviewer> Reviewers { get; set; }  
+
+
+    }
+}
