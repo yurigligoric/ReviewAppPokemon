615612837bc09fa87888514eda906eaed48ee53d
yurigligoric <yurigligoric@gmail.com>
2023-01-09 23:48:27 +0800

    before the migration

--- Models/Review.cs
+++ Models/Review.cs
@@ -8,6 +8,8 @@ namespace ReviewApp.Models
         public string Title { get; set; }
         public string Text { get; set; }
 
+        public int Rating { get; set; } 
+
         public Reviewer Reviewer { get; set; }
         public Pokemon Pokemon { get; set; }
     }
